a035b4388b38450f7603b878be0ab1335765dcd7
PauG <paugaston@hotmail.com>
2026-01-24 13:45:59 +0100

    feat: CreatePlayerService implemented.

---
+++ src/main/java/cat/itacademy/blackjack/player/application/usecase/CreatePlayerService.java
@@ -0,0 +1,31 @@
+package cat.itacademy.blackjack.player.application.usecase;
+
+import cat.itacademy.blackjack.game.domain.model.Game;
+import cat.itacademy.blackjack.game.domain.port.in.CreateGameUseCase;
+import cat.itacademy.blackjack.game.domain.port.out.PlayerRepository;
+import cat.itacademy.blackjack.player.domain.model.Player;
+import cat.itacademy.blackjack.player.domain.port.in.CreatePlayerUseCase;
+import cat.itacademy.blackjack.player.domain.port.in.PlayerResponse;
+import reactor.core.publisher.Mono;
+
+public class CreatePlayerService implements CreatePlayerUseCase {
+
+    private PlayerRepository playerRepository;
+
+    public CreatePlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @Override
+    public Mono<PlayerResponse> create(String playerName) {
+
+        Player player = Player.create(playerName);
+
+        return playerRepository.save(player)
+                .map(savedPlayer -> new PlayerResponse(
+                        savedPlayer.id().value(),
+                        savedPlayer.name(),
+                        savedPlayer.score()
+                ) );
+    }
+}
