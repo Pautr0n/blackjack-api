f1293faad2b93bfb0d4fb0b008146f998db87507
PauG <paugaston@hotmail.com>
2026-01-24 20:03:46 +0100

    refactor: changing arguments on findById and deleteById, in DDD repository works with domain's entities and not with primitive data type.

--- src/main/java/cat/itacademy/blackjack/game/domain/port/out/GameRepository.java
+++ src/main/java/cat/itacademy/blackjack/game/domain/port/out/GameRepository.java
@@ -1,14 +1,15 @@
 package cat.itacademy.blackjack.game.domain.port.out;
 
 import cat.itacademy.blackjack.game.domain.model.Game;
+import cat.itacademy.blackjack.game.domain.model.GameId;
 import reactor.core.publisher.Mono;
 
 public interface GameRepository {
 
     Mono<Game> save(Game game);
 
-    Mono<Game> findById(String id);
+    Mono<Game> findById(GameId id);
 
-    Mono<Void> deleteById(String id);
+    Mono<Void> deleteById(GameId id);
 
 }
