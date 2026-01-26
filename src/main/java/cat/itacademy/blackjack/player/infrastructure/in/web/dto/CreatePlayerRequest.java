2249eb4867096ebdd22ecc0893ed61d342e38d88
PauG <paugaston@hotmail.com>
2026-01-26 21:08:33 +0100

    refactor: adding validations to Player domain requests

--- src/main/java/cat/itacademy/blackjack/player/infrastructure/in/web/dto/CreatePlayerRequest.java
+++ src/main/java/cat/itacademy/blackjack/player/infrastructure/in/web/dto/CreatePlayerRequest.java
@@ -1,7 +1,14 @@
 package cat.itacademy.blackjack.player.infrastructure.in.web.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "Request to create a new player")
-public record CreatePlayerRequest( @Schema(example = "Pau") String name) {
+public record CreatePlayerRequest(
+        @NotBlank(message = "Player name cannot be blank")
+        @Size(min = 3, message = "Player name must have at least 3 characters")
+        @Schema(example = "Pau", description = "The name of the player")
+        String name
+) {
 }
--- src/main/java/cat/itacademy/blackjack/player/infrastructure/in/web/dto/RenamePlayerRequest.java
+++ src/main/java/cat/itacademy/blackjack/player/infrastructure/in/web/dto/RenamePlayerRequest.java
@@ -1,7 +1,13 @@
 package cat.itacademy.blackjack.player.infrastructure.in.web.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "Request to rename a player")
-public record RenamePlayerRequest(@Schema(example = "newName") String newName) {
+public record RenamePlayerRequest(
+        @NotBlank(message = "Player name cannot be blank")
+        @Size(min = 3, message = "Player name must have at least 3 characters")
+        @Schema(example = "Pau", description = "The name of the player") String newName
+) {
 }
